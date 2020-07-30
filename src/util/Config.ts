43c4e4dd89014640541604910a5ee2eed0dbd304
Julian <info@racaps.com>
2020-07-30 18:53:17 -0300

    Arreglos

--- src/util/Config.ts
+++ src/util/Config.ts
@@ -2,7 +2,7 @@ import dontenv from 'dotenv'
 dontenv.config()
 
 const {PORT, NODE_ENV, MONGODB, DB_NAME, SECRETA} = process.env
-if (!PORT || !NODE_ENV || !MONGODB || !DB_NAME || SECRETA) {
+if (!PORT || !NODE_ENV || !MONGODB || !DB_NAME || !SECRETA) {
   console.log('Se tiene que crear un archivo .env con las siguiente configuración')
   console.log(
     'PORT=5050\n' +
